8ada7a76fbe3ddc4a545a1a88205ccd7330ec9cd
Park Na Yeon <within197@naver.com>
2021-10-06 16:53:50 +0900

    배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  // user setting
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  // user info
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
@@ -18,13 +34,33 @@ function App() {
       username: "liz",
       email: "liz@example.com",
     },
-  ];
+  ]);
 
-  const nextId = useRef(4);
+  const nextId = useRef(4); // current 속성 : 4
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]); //spread 연산자로 리스트 추가
+    setInputs({
+      username: "",
+      email: "",
+    }); //  value 초기화
     nextId.current += 1;
   };
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />;
+    </>
+  );
 }
 
 export default App;
